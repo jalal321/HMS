18b51a9fcc7e87705449f7a8fbeffb5c5db5d290
jalal321 <ahsyou1@gmail.com>
2020-07-18 07:14:07 -0700

    added basic enteties

---
+++ HMS.Entities/Booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    class Booking
+    {
+        public int Id { get; set; }
+
+        public int AccomodationId { get; set; }
+
+        public Accomodation Accomodation { get; set; }
+
+       
+        public DateTime FromDate { get; set; }
+
+        /// <summary>
+        /// no of stay nights
+        /// </summary>
+        public int Duration { get; set; }
+
+    }
+}
