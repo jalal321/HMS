04253357be9795c42e0bcee436a6c381b73b5570
jalal321 <ahsyou1@gmail.com>
2020-07-20 09:38:56 -0700

    added HMS.services

--- HMS.Data/HMSContext.cs
+++ HMS.Data/HMSContext.cs
@@ -6,16 +6,18 @@ using System.Threading.Tasks;
 using HMS.Entities;
 
 
+
 namespace HMS.Data
 {
     public class HMSContext : DbContext
     {
 
-        public HMSContext(): base("HMSConnectionString")
+        public HMSContext()
+            : base("HMSConnectionString")
         {
                 
         }
-
+         
         public DbSet<Accomodation> Accomodations { get; set; }
         public DbSet<AccomodationPackage> AccomodationPackages { get; set; }
         public DbSet<AccomodationType> AccomodationTypes { get; set; }
---
+++ HMS/Areas/DashBoard/DashBoardAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace HMS.Areas.DashBoard
+{
+    public class DashBoardAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DashBoard";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DashBoard_default",
+                "DashBoard/{controller}/{action}/{id}",
+                new { Controller = "DashBoard", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
