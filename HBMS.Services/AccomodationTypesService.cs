fbc51d6beb67938ec74785bcfdf5f0e9c2431dc4
jalal321 <ahsyou1@gmail.com>
2020-07-21 08:18:17 -0700

    Revert "Revert "viewmodels saved""
    This reverts commit c5fad25e55f23e23030ba18f7e63be7472b4bf0c.

---
+++ HBMS.Services/AccomodationTypesService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMS.Data;
+using HMS.Entities;
+
+namespace HMS.Services
+{
+   public class AccomodationTypesService
+    {
+
+       public IEnumerable<AccomodationType> GetAllAccomodationTypes()
+       {
+           var context = new HMSContext();
+           return context.AccomodationTypes.ToList(); 
+       }
+    }
+}
